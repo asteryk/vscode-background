4aa9140086f20363c855e8ad0cd615ceca71f8e8
liangyuekang <liangyuekang@163.com>
2016-08-12 16:02:59 +0800

    change:做了微小的改动，支持mac

--- src/background.js
+++ src/background.js
@@ -6,25 +6,28 @@ var vshelp = require('./vshelp');
 var path = require('path');
 var fs = require('fs');
 var child = require('child_process');
-
-var dirPath = "";     //主要文件夹路径
-var indexPath = "";   //electron入口html
+var os = require('os');
+var dirPath = ""; //主要文件夹路径
+var indexPath = ""; //electron入口html
 
 function search() {
     vshelp.showStatusBar('searching...', 3000);
-
     return new Promise((resolve, reject) => {
-        child.exec('reg query HKEY_CLASSES_ROOT\\*\\shell\\VSCode /v Icon', function (error, strOut, strError) {
+        child.exec('reg query HKEY_CLASSES_ROOT\\*\\shell\\VSCode /v Icon', function(error, strOut, strError) {
             vshelp.showStatusBar('Search is complated.', 3000);
             if (error) {
                 console.log(error);
-                vshelp.showStatusBar('Query error!  Please enter the path by yourself.(没找到安装目录，请自己输入吧...)', 10000);
+                if (os.platform !== 'win32') {
+                    vshelp.showStatusBar('not win os,Please enter the path by yourself.(非win，请自己输入吧...)', 10000);
+                } else {
+                    vshelp.showStatusBar('Query error!  Please enter the path by yourself.(没找到安装目录，请自己输入吧...)', 10000);
+                }
                 reject(error);
                 return;
             }
 
             var exePath = strOut.match(/REG_EXPAND_SZ\s+(\S[\s\S]*exe)/)[1];
-            var installPath = path.dirname(exePath || '');        //获取安装目录
+            var installPath = path.dirname(exePath || ''); //获取安装目录
 
             resolve(installPath);
         });
@@ -32,12 +35,12 @@ function search() {
 
 }
 
-function init(value, arr, noAction) {      //逻辑入口
+function init(value, arr, noAction) { //逻辑入口
 
     return vshelp.prompt({
         value: value || "",
         prompt: "Please enter the path of your vscode (请输入您的vscode安装目录)",
-        placeHolder: "for example(比如)： E:\\Microsoft VS Code"
+        placeHolder: "for example(比如)： E:\\Microsoft VS Code，/Users/YourID/.../VSCode.app"
     }).then(baseUrl => {
         if (!baseUrl) return;
 
@@ -56,9 +59,17 @@ function init(value, arr, noAction) {      //逻辑入口
 }
 
 
-function judgePath(inputPath) {  //校验并填充路径
+function judgePath(inputPath) { //校验并填充路径
 
-    var _dirPath = path.join(inputPath, 'resources\\app\\out\\vs\\workbench\\electron-browser');
+    switch (os.platform) {
+        case 'win32':
+            var _dirPath = path.join(inputPath, 'resources\\app\\out\\vs\\workbench\\electron-browser');
+            break;
+        default:
+        case 'darwin': //mac os
+            var _dirPath = path.join(inputPath, 'Contents/resources/app/out/vs/workbench/electron-browser');
+            break;
+    }
 
     if (!judgeDirPath(_dirPath)) return false;
     dirPath = _dirPath;
@@ -72,27 +83,27 @@ function judgePath(inputPath) {  //校验并填充路径
 }
 
 
-function judgeDirPath(dirPath) {                 //判断main文件夹是否存在
+function judgeDirPath(dirPath) { //判断main文件夹是否存在
     var hasDir = fs.existsSync(dirPath);
 
-    if (!hasDir) {  //路径不合法
+    if (!hasDir) { //路径不合法
         vshelp.showError("Something wrong with the path you gave.")
     }
 
     return hasDir;
 }
 
-function judgeIndexPath(indexPath) {              //判断index.html 是否存在
+function judgeIndexPath(indexPath) { //判断index.html 是否存在
     var hasFile = fs.existsSync(indexPath);
 
-    if (!hasFile) {  //路径不合法
+    if (!hasFile) { //路径不合法
         vshelp.showError("Your index.html missing!!!")
     }
 
     return hasFile;
 }
 
-function fillInScript() {               //注入脚本到index.html
+function fillInScript() { //注入脚本到index.html
     var html = fs.readFileSync(indexPath, 'utf-8');
 
     html = html.replace(/<!--extstart-->[\s\S]*?<!--extend-->/, '');
@@ -102,7 +113,7 @@ function fillInScript() {               //注入脚本到index.html
     fs.writeFileSync(indexPath, html);
 }
 
-function removeTheScript() {            //移除index.html的脚本文件
+function removeTheScript() { //移除index.html的脚本文件
     var html = fs.readFileSync(indexPath, 'utf-8');
 
     html = html.replace(/<!--extstart-->[\s\S]*?<!--extend-->/, '');
@@ -120,15 +131,15 @@ function uninstall() {
     });
 }
 
-function createLoader(arr) {            //创建脚本入口文件
+function createLoader(arr) { //创建脚本入口文件
     var html = fs.readFileSync(path.join(__dirname, 'ext-loader.js'), 'utf-8');
     var index = 0;
     if (!arr) {
         arr = require('./defSrc');
     }
 
-    if (arr && arr.length) {                                                //修改background-image
-        html = html.replace(/url\((\S*?)\)/g, function (g0, g1) {
+    if (arr && arr.length) { //修改background-image
+        html = html.replace(/url\((\S*?)\)/g, function(g0, g1) {
             var url = arr[index++];
             return url ? `url(${url})` : 'none';
         });
@@ -140,8 +151,8 @@ function createLoader(arr) {            //创建脚本入口文件
 
 module.exports = {
     vshelp: vshelp,
-    search: search,  //搜索安装目录
-    init: init,      //初始化
+    search: search, //搜索安装目录
+    init: init, //初始化
     // setImg: setImg,   //设置图片地址
-    uninstall: uninstall  //移除index中的脚本
+    uninstall: uninstall //移除index中的脚本
 };
